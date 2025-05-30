1e2ee94d19342c71b1907b6225b6acd5936133a3
David Oliveira <dedepco@gmail.com>
2025-05-30 19:18:09 -0300

    feature: add migrations

--- src/domain/entities/messageHistory.entity.ts
+++ src/domain/entities/messageHistory.entity.ts
@@ -1,10 +1,24 @@
 import { MessageStatus } from 'src/shared/enum';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 export class MessageHistory {
+  @PrimaryGeneratedColumn()
   id: string;
+
+  @Index()
+  @PrimaryGeneratedColumn('uuid')
   messageId: string;
+
+  @Column()
   status: MessageStatus;
+
+  @Column({ type: 'text', nullable: true })
   error?: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   timestamp: Date;
+
+  @Column({ type: 'int', default: 0 })
   retries: number;
 }
